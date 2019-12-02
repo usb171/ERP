bee6e6b9c0572d0ccc9ec970733e884c35290938
cidadeverde <usb171@gmail.com>
2019-12-01 22:21:04 -0300

    Finalizado as interações da tabela do paciente

--- paciente/funcoes/paciente.py
+++ paciente/funcoes/paciente.py
@@ -1,5 +1,8 @@
 from ..models import Paciente as PacienteModel
-
+from django.core.paginator import Paginator
+from django.db.models import Q
+import operator
+import functools
 
 def criar(formulario):
     try:
@@ -58,3 +61,50 @@ def criarEditarExcluir(request):
         return {'status': False, 'msg': ['Não foi possível executar o comando: ' + str(comando)]}
 
 
+def getColunasTabela():
+    colunas = [
+                {'html': '<th style="width:5%">Cód.</th>', 'nomeVariavel': 'id'},
+                {'html': '<th style="width:30%">Nome</th>', 'nomeVariavel': 'nomeCompleto'},
+                {'html': '<th style="width:20%">Whatsapp</th>', 'nomeVariavel': 'whatsapp'},
+                {'html': '<th style="width:20%">Telefône</th>', 'nomeVariavel': 'telefone'},
+                {'html': '<th style="width:25%">E-mail</th>', 'nomeVariavel': 'email'}
+              ]
+    return colunas
+
+def getColunasTabelaHtml():
+    colunas = ''
+    for coluna in list(map(operator.itemgetter('html'), getColunasTabela())):
+        colunas = colunas + coluna
+    return '<tr>{0}</tr>'.format(colunas)
+
+def listarTabela(request):
+
+    busca = request.GET.get('search[value]')
+    start = request.GET.get('start')
+    length = request.GET.get('length')
+
+    pacientes_total = PacienteModel.objects.all()
+    pacientes_filtro = pacientes_total.filter((Q(nomeCompleto__contains=busca.upper()) | Q(email__contains=busca)))
+
+    '''Bloco da ordenação das colunas'''
+    try:
+        coluna = int(request.GET.get('order[0][column]'))
+        tipo_ordem = request.GET.get('order[0][dir]')
+
+        print(coluna)
+
+        if tipo_ordem == 'desc':
+            pacientes_filtro = pacientes_filtro.order_by(getColunasTabela()[coluna]['nomeVariavel'])
+        elif tipo_ordem == 'asc':
+            pacientes_filtro = pacientes_filtro.order_by('-' + getColunasTabela()[coluna]['nomeVariavel'])
+    except:
+        pacientes_filtro = pacientes_filtro.order_by('nomeCompleto')
+
+    pacientes_filtro = pacientes_filtro.values('id', 'nomeCompleto', 'whatsapp', 'telefone', 'email')
+
+    pagina = Paginator(pacientes_filtro, length)
+    paginaAtual = int(start) / int(length) + 1
+    pacientes_filtro = pagina.page(paginaAtual)
+    pacientes_filtro = list(map(lambda paciente: list(paciente.values()), pacientes_filtro))
+    total_pacientes = len(pacientes_total)
+    return {"recordsTotal": total_pacientes, "recordsFiltered": total_pacientes, "data": pacientes_filtro}
